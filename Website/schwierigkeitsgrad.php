17eb01908bf2287d36ad8f721ff7155a256e71c0
Nicolas Matheisen <nicolasmatheisen@icloud.com>
2026-05-06 23:54:45 +0200

    css klassen neu organisiert

--- Website/schwierigkeitsgrad.php
+++ Website/schwierigkeitsgrad.php
@@ -16,20 +16,14 @@
                         <span class="Schwierigkeitsgrad">Schwierigkeitsgrad Leicht</span>
                         <span class="Versuchsanzahl">20 Versuche</span>
                     </div>
-                </div>
-                <div class="Schwierigkeitsgrad-div">
                     <div class="Schwierigkitsgrad-Anzeige">
                         <span class="Schwierigkeitsgrad">Schwierigkeitsgrad Mittel</span>
                         <span class="Versuchsanzahl">10 Versuche</span>
                     </div>
-                </div>
-                <div class="Schwierigkeitsgrad-div">
                     <div class="Schwierigkitsgrad-Anzeige">
                         <span class="Schwierigkeitsgrad">Schwierigkeitsgrad Schwer</span>
                         <span class="Versuchsanzahl">5 Versuche</span>
                     </div>
-                </div>
-                <div class="Schwierigkeitsgrad-div">
                     <div class="Schwierigkitsgrad-Anzeige">
                         <span class="Schwierigkeitsgrad">Schwierigkeitsgrad Extrem</span>
                         <span class="Versuchsanzahl">3 Versuche</span>
